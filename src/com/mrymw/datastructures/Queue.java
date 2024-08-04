06a511bcf4fa76c1d9ec7cce23b987291094defe
Maryam <mrymcodes@gmail.com>
2024-08-04 11:24:45 +0300

    queue class updated

--- src/com/mrymw/datastructures/Queue.java
+++ src/com/mrymw/datastructures/Queue.java
@@ -1,5 +1,35 @@
 package com.mrymw.datastructures;
 
-public class Queue {
+import com.mrymw.exception.EmptyQueueException;
+import com.mrymw.exception.FullQueueException;
+
+public class Queue <E> {
+    protected int capacity;
+    protected  E[] queue;
+    int frontIndex; //index of the first element
+    int rearIndex; //index of the next element
+
+    public Queue(int capacity) {
+        this.capacity = capacity;
+        queue = (E[]) new Object[capacity];
+        frontIndex = rearIndex = 0;}
+    public int size(){return (capacity - (frontIndex + rearIndex)) % capacity;}
+    public boolean isEmpty() {return (frontIndex == rearIndex);}
+    public E front() throws EmptyQueueException {
+        if(isEmpty()) {throw  new EmptyQueueException("Queue is EMPTY!");}
+        return queue[frontIndex];
+    }
+    public void enqueue(E element) throws FullQueueException {
+        if(size() == capacity-1) throw new FullQueueException("Queue is FULL!");
+        queue[rearIndex] = element;
+        rearIndex = (rearIndex + 1) % capacity;
+    }
+    public E dequeue() throws EmptyQueueException {
+        if(isEmpty()) throw new EmptyQueueException("Queue is EMPTY!");
+        E temp = queue[frontIndex];
+        queue[frontIndex] = null;
+        frontIndex = (frontIndex + 1) % capacity;
+        return temp;
+    }
 
 }
